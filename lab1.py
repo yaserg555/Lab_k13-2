4fd8d389aa6218c577265a781614168a32a74927
Nevedrov <71889912+Nevedrov@users.noreply.github.com>
2020-10-01 22:13:15 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,24 @@
+print('Алексей')
+def cout(a, b):
+    print(str(a)+'='+str(b))
+
+
+cout('1+2', 1 + 2)
+cout('(2+2*2', 2 + 2 * 2)
+cout('2+2**2**2', 2 + 2 ** 2 ** 2)
+cout('2+2**2**2', 2 + 2 ** 2 ** 2)
+cout('2**2**-2-1', 2 ** 2 ** -2 - 1)
+cout('type(2**2**-2-1)', type(2**2**-2-1))
+cout('type(2/2)', type(2/2))
+cout('type(2//2) ', type(2//2))
+cout('type(5//2) ', type(5//2))
+cout('5%2 ', 5 % 2)
+cout('4%2 ', 4 % 2)
+cout('5%2.5', 5 % 2.5)
+cout('5%-2.5 ', 5 % -2.5)
+a = 15
+print('a=15')
+cout('bin(a) ', bin(a))
+cout('hex(a)', hex(a))
+print(int('0b1111', 2))
+print(int('0xf', 16))
