8cf43bc8ca73d41ea63b5bdcf66685459a9d6876
korven4 <71665342+korven4@users.noreply.github.com>
2020-10-08 18:14:24 +0300

    Update and rename 2.py to lab1.2.py

---
+++ Vasiclhenko/lab1.2.py
@@ -0,0 +1,23 @@
+v = int(input('v'))
+print('a')
+a= v%10
+print(a)
+print('б')
+b = ''
+while v > 0:
+    b = str(v % 2) + b
+    v = v // 2
+b=int(b)
+print(b%10)
+
+#a=10.001**345
+#b=13.001**249
+#c=9.001**355
+#d=11.001**209
+#x=a*b/(c*d)
+#print(x)
+
+print('Виконавець: Васильченко')
+print('            Юрiй')
+print('            Сергiйович')
+print('     Група: К-13(2)')
