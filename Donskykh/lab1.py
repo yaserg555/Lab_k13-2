a2e59b451e5fb743bb2caeaeb27e4dbeb5afc58b
Donskykh_Volodimir <71886261+VDblue@users.noreply.github.com>
2020-10-09 00:17:08 +0300

    Update lab1.py

--- Donskykh/lab1.py
+++ Donskykh/lab1.py
@@ -1,3 +1,41 @@
 print("Donskykh")
-askdjfhhjksadgvjhbaxjchklv
-лорфклопф
+int('100011100011', 10) #100011100011
+int('100011100011', 2)#2275
+int('100011100011', 4)#4215813
+int('100011100011', 8)#8592326665
+int('100011100011', 16)#17592472305681
+a=int(input())
+a=100
+int(a,2)#<class 'int'>
+#Traceback (most recent call last):
+#  File "<pyshell#32>", line 1, in <module>
+#    int(a, 2)
+#TypeError: int() can't convert non-string with explicit base
+#такая вот ошибка прикол
+type(a) 
+ok
+a=str(a)
+type(a) #<class 'str'>
+int(a, 2)
+#перемога
+a='FAFBCD'
+int(a,16) #16448461
+
+
+
+type(9/5)#<class 'float'>
+type(9/3)#<class 'float'>
+type(9%3)#<class 'int'>
+import this #ок это классная пасхалка
+#Now is better than never. моя отговорка для этого убитого дедлайна
+import math
+math.pi#3.141592653589793
+type(math.pi)
+#<class 'float'>
+math.sqrt(2)
+#1.4142135623730951
+3.15**1.5#5.590695395029137
+3.15**1.5==.590695395029137 #true
+math.sqrt(2)==1.4142135623730951#true
+3**1.5==5.196152422706632 #true
+#fun fact # 3**1.5==5.196152422706632
