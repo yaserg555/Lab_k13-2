f4045bbfa4d4797ceec1f25a6f18292902e10ec7
Nikita <45831329+gaspar62@users.noreply.github.com>
2020-10-08 17:35:31 +0300

    Update lab1.py

--- Gasparian/lab1.py
+++ Gasparian/lab1.py
@@ -1,1 +1,57 @@
 print('gasparian')
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+a = 9/5
+print("9/5 = " + str(a))
+print(type(a))
+b = (-9)/5
+print("(-9)/5 = " + str(b))
+print(type(b))
+c = 9/(-5)
+print("9/(-5) = " + str(c))
+print(type(c))
+d = (-9)/(-5)
+print("(-9)/(-5) = " + str(d))
+print(type(d))
+p = 9//5
+print("9//5 = " + str(p))
+print(type(p))
+f = (-9)//5
+print("(-9)//5 = " + str(f))
+print(type(f))
+g = 9//(-5)
+print("9//(-5) = " + str(g))
+print(type(g))
+h = (-9)//(-5)
+print("(-9)//(-5) = " + str(h))
+print(type(h))
+i = 9%5
+print("9%5 = " + str(i))
+print(type(i))
+j = (-9)%5
+print("(-9)%5 = " + str(j))
+print(type(j))
+k = 9%(-5)
+print("9%(-5) = " + str(k))
+print(type(k))
+l = (-9)%(-5)
+print("(-9)%(-5) = " + str(l))
+print(type(l))
+n = 9.0/5.0
+print("9.0/5.0 = " + str(n))
+print(type(n))
+m = 9.0//5.0
+print("9.0//5.0 = " + str(m))
+print(type(m))
+o = 1.5//0.75
+print("1.5//0.75 = " + str(o))
+print(type(o))
+
+import math
+print(math.sqrt(2))
+"""1.41..."""
+print(3 ** 1.5)
+"""5.19..."""
