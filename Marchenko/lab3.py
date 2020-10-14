7c988560e71cfcdd39fc20657f836912874220d4
marchenkov25 <71882306+marchenkov25@users.noreply.github.com>
2020-10-14 13:55:54 +0300

    Create lab3.py

---
+++ Marchenko/lab3.py
@@ -0,0 +1,44 @@
+from math import tan, pi, atan, log, exp
+from datetime import datetime
+print('-1-')
+day = str(input()).zfill(2)
+month = str(input()).zfill(2)                                                                        # x, y = 1, 2; (x + y)/(x ** 2 + y ** 2 + 1) = 0.5
+year = str(input())[-2:]
+if int(year) % 4 != 0 and int(month) == 2 and int(day) == 29:
+    print(f'Такої дати не існує')
+elif int(month) == 2 and (int(day) == 30 or int(day) == 31):
+    print('Такої дати не існує')
+elif 1 <= int(day) <= 31 and 1 <= int(month) <= 12:
+    print(f'{day}.{month}.{year}')
+else:
+    print('Такої дати не існує')
+print('-2-')
+x = float(input())                                            
+y = float(input())
+z = 1 / tan(x + y)
+print(f'ctg(x+y)={z:.2e}')
+print('-3-')
+x = float(input())
+y = pi / 2 - atan(x)
+print(f'arcctg(x)={y:.4f}')
+print('-4-')
+x = (exp(log(10.001/9.001) * 345) * exp(log(13.001/11.001) * 249)) / (exp(log(9.001) * 10) * exp(log(11.001) * 20))
+print(f'(10.001^345*13.0001^249)/(9.001^355*11.001^269)={x}')
+
+now = datetime.now()
+then = datetime(2020, 9, 21, 8, 40, 0)
+future = datetime(2024, 6, 30, 12, 0)
+dif1 = now - then
+dif2 = future - now
+print('Минуло:')
+print('тижні: ' + str(dif1.days // 7))
+print('дні: ' + str(dif1.days))
+print('години: ' + str(dif1.days * 24 + dif1.seconds // 3600))
+print('хвилини: ' + str(dif1.days * 1440 + dif1.seconds // 60))
+print('секунди: ' + str(dif1.days * 86400 + dif1.seconds))
+print('Залишилось:')
+print('тижні: ' + str(dif2.days // 7))
+print('дні: ' + str(dif2.days))
+print('години: ' + str(dif2.days * 24 + dif2.seconds // 3600))
+print('хвилини: ' + str(dif2.days * 1440 + dif2.seconds // 60))
+print('секунди: ' + str(dif2.days * 86400 + dif2.seconds))
