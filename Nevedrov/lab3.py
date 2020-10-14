c609740dc5ad2421abf2d716b7dae4542086acfe
Nevedrov <71889912+Nevedrov@users.noreply.github.com>
2020-10-14 19:23:41 +0300

    Update lab3.py
    Виконавець: Нєвєдров
                Олексій
                Дмитрович
         Група: К-13(2)
         
    Minutes:gg
    Hours:ghfdfh
    Invalid time
    Minutes:11
    Hours:11
    Time: 11:11
    Day:fdgfd
    Month:dfg
    Year:dfg
    Invalid date
    Day:123
    Month:11
    Year:222222
    Invalid date
    Day:11
    Month:11
    Year:2020
    Date: 11.11.2020
    Print  x:1
    Print  y:1
    ctg(x+y) = -4.58e-01
    x+y/(x^2+y^2+1) = 0.6666666666666666
    x+y/(x^2+y^2+1) = 6.667e-01
    Print x:1
    arcctg(x) = 0.7854
    exp(ln(10.001/9.001)^345*exp(ln(13.001/11.001)^249/9.001^10/11.001^20 = 2997.246914575195
    Seconds remains: 3349348.2029161453 and 154417051.79708385
    Minutes remains: 55823 and 2573618
    Days remains: 39 and 1788
    Weeks remains: 6 and 256
    exp(ln(10.001/9.001))^345*exp(ln(13.001/11.001))^249/9.001^10/11.001^20 execution speed  = 1.0766198000000031
    exp(ln((10.001/9.001)^345*(13.001/11.001)^249/9.001^10/11.001^20)) execution speed = 0.25009989999999505
    arccos(cos(((10.001/9.001)^345*(13.001/11.001)^249/9.001^10/11.001^20))) execution speed = 0.24870169999999803
    (10.001/9.001)^345*(13.001/11.001)^249/9.001^10/11.001^20 execution speed = 0.007103600000000654
    
    Process finished with exit code 0

--- Nevedrov/lab3.py
+++ Nevedrov/lab3.py
@@ -1,6 +1,6 @@
 from math import pi, e, tan, atan, log, exp, ceil
 from timeit import timeit, Timer
-from time import time, mktime
+from time import time, mktime, strptime
 
 
 def cout(a, b):
@@ -8,7 +8,37 @@ def cout(a, b):
 
 
 def cout2(a, b, c):
-    print(str(a)+':'+' '+str(b)+' '+'and'+' '+str(c))
+    if c == 0:
+        print(str(a) + ':' + ' ' + str(b))
+    else:
+        print(str(a) + ':' + ' ' + str(b) + ' ' + 'and' + ' ' + str(c))
+
+
+def time_input():
+    m = input('Minutes:')
+    h = input('Hours:')
+    if m.isdigit() == 1 and h.isdigit() == 1:
+        m = int(m)
+        h = int(h)
+        if m < 60 and h < 24:
+            cout2('Time', f'{h}:{m}', 0)
+        else:
+            print('Invalid time')
+            time_input()
+    else:
+        print('Invalid time')
+        time_input()
+
+def data_input():
+    dd = input('Day:')
+    mm = input('Month:')
+    yy = input('Year:')
+    try:
+        valid_date = strptime(f'{mm}/{dd}/{yy}', '%m/%d/%Y')
+        cout2('Date', f'{dd}.{mm}.{yy}', 0)
+    except ValueError:
+        print('Invalid date')
+        data_input()
 
 
 def acot(a):
@@ -37,34 +67,37 @@ print('''
             Дмитрович
      Група: К-13(2)
      ''')
+
 # 1
-dd = input('Day:')
-mm = input('Month:')
-yy = input('Year:')
-cout('dd.mm.yy', f'{dd}.{mm}.{yy}')
+time_input()
+data_input()
 # 2
 x = float(input('Print  x:'))
 y = float(input('Print  y:'))
 ctg = 1/tan(x+y)
 cout('ctg(x+y)', '{:.2e}'.format(ctg))
+cout('x+y/(x^2+y^2+1)', (x+y)/(x**2+y**2+1))
+cout('x+y/(x^2+y^2+1)', '{:.3e}'.format((x+y)/(x**2+y**2+1)))
 # 3
 x2 = float(input('Print x:'))
 cout('arcctg(x)', '{:.4f}'.format((acot(x2))))
 # 4
-cout('exp(log(10.001/9.001, e))**345*exp(log(13.001/11.001, e))**249/9.001**10/11.001**20', exp(log(10.001/9.001, e))**345*exp(log(13.001/11.001, e))**249/9.001**10/11.001**20)
+cout('exp(ln(10.001/9.001)^345*exp(ln(13.001/11.001)^249/9.001^10/11.001^20', exp(log(10.001/9.001, e))**345*exp(log(13.001/11.001, e))**249/9.001**10/11.001**20)
 # 5
-enrollment_date = (2020, 9, 6, 0, 0, 0, 0, 0, 0)                             # дата зачисления
-end_date = (2025, 9, 6, 0, 0, 0, 0, 0, 0)                                    # дата выпуска
+enrollment_date = (2020, 9, 6, 0, 0, 0, 0, 0, 0)                                     # дата зачисления
+end_date = (2025, 9, 6, 0, 0, 0, 0, 0, 0)                                            # дата выпуска
 current_time_interval = time()
 past_time_interval = current_time_interval - mktime(enrollment_date)
 future_time_interval = mktime(end_date) - current_time_interval
-cout2('seconds', past_time_interval, future_time_interval)                    # секунды
-cout2('minutes', minutes(past_time_interval), minutes(future_time_interval))  # минуты
-cout2('days', days(past_time_interval), days(future_time_interval))           # дни
-cout2('weeks', weeks(past_time_interval), weeks(future_time_interval))        # недели
+cout2('Seconds remains', past_time_interval, future_time_interval)                    # секунды
+cout2('Minutes remains', minutes(past_time_interval), minutes(future_time_interval))  # минуты
+cout2('Days remains', days(past_time_interval), days(future_time_interval))           # дни
+cout2('Weeks remains', weeks(past_time_interval), weeks(future_time_interval))        # недели
 # 6
 t1 = Timer('exp(log(10.001/9.001, e))**345*exp(log(13.001/11.001, e))**249/9.001**10/11.001**20', "from math import exp, log, e")
-t2 = Timer('exp(log((10.001/9.001)**345*(13.001/11.001)**249/9.001**10/11.001**20, e))', "from math import exp, log, e")
-cout('exp(log(10.001/9.001, e))**345*exp(log(13.001/11.001, e))**249/9.001**10/11.001**20 execution speed ', t1.timeit())
-cout('exp(log((10.001/9.001)**345*(13.001/11.001)**249/9.001**10/11.001**20, e) execution speed', t2.timeit())
-cout('(10.001/9.001)**345*(13.001/11.001)**249/9.001**10/11.001**20 execution speed', timeit('(10.001/9.001)**345*(13.001/11.001)**249/9.001**10/11.001**20'))
+t2 = Timer('exp(log((10.001/9.001)**345*(13.001/11.001)**249/9.001**10/11.001**20))', "from math import exp, log, e")
+t3 = Timer('acos(cos(((10.001/9.001)**345*(13.001/11.001)**249/9.001**10/11.001**20)))', "from math import cos, acos")
+cout('exp(ln(10.001/9.001))^345*exp(ln(13.001/11.001))^249/9.001^10/11.001^20 execution speed ', t1.timeit())
+cout('exp(ln((10.001/9.001)^345*(13.001/11.001)^249/9.001^10/11.001^20)) execution speed', t2.timeit())
+cout('arccos(cos(((10.001/9.001)^345*(13.001/11.001)^249/9.001^10/11.001^20))) execution speed', t3.timeit())
+cout('(10.001/9.001)^345*(13.001/11.001)^249/9.001^10/11.001^20 execution speed', timeit('(10.001/9.001)**345*(13.001/11.001)**249/9.001**10/11.001**20'))
