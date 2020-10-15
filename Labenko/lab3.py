0690eb2ca86f8d8065ebcb48df8ee1d2b7f27f57
Annalab-sys <72043120+Annalab-sys@users.noreply.github.com>
2020-10-15 18:27:26 +0300

    Update lab3.py

--- Labenko/lab3.py
+++ Labenko/lab3.py
@@ -2,3 +2,19 @@ day = input("Виберіть число ")
 month = input("Виберіть місяць ")
 year = input("Виберіть рік ")
 print(f"дата: {day}.{month}.{year}")
+
+x = input("Введіть перше значення ")
+y = input("Введіть друге значення ")
+import math
+z = math.cos (float(x+y)) / math.sin (float(x+y))
+print("%.2f" % z)
+
+a = int(input("Введіть значення "))
+import math
+if a != 0:
+    b = math.atan (1/a)
+    print("%.4f" % b)
+else:
+    print("1.5708")
+
+    
