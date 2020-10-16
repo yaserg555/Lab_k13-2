23a401449d9a8b0607833b4beb7d7a0fdcc2edbc
Nevedrov <71889912+Nevedrov@users.noreply.github.com>
2020-10-16 13:37:22 +0300

    Update sam_Nevedrov.py

--- Nevedrov/sam_Nevedrov.py
+++ Nevedrov/sam_Nevedrov.py
@@ -17,3 +17,26 @@ if A + B == C or B + C == A or A + C == B and A >= 0 and B >= 0 and C >= 0:
     print('yes')
 else:
     print('no')
+#7
+def check(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+def check2(a):
+    if a % 2 == 1:
+        return 0
+    else:
+        return a
+
+
+a = int(input('A:'))
+b = int(input('B:'))
+c = int(input('C:'))
+d = int(input('D:'))
+if a%2 == 1 and b%2 == 1 and c%2==1 and d%2 == 1:
+    print('not found')
+else:
+    print(check(check(check(check2(a), check2(b)), check2(c)), check2(d)))
