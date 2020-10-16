317598d8ab481014d1250ecec2983a3a242b9b58
Annalab-sys <72043120+Annalab-sys@users.noreply.github.com>
2020-10-16 12:42:52 +0300

    Update sam-Labenko.py

--- Labenko/sam-Labenko.py
+++ Labenko/sam-Labenko.py
@@ -1,7 +1,10 @@
 x = float(input("Перша сторона трикутника "))
 y = float(input("Друга сторона трикутника "))
 z = float(input("Третя сторона трикутника "))
-s = x + y + z
+import math
+p = float(x+y+z/2)
+s = math.sqrt(p*(p-x)*(p-y)*(p-z))
+
 if x > y+z:
     print("Помилка")
 elif y > z+x:
@@ -10,6 +13,9 @@ elif z > x+y:
     print("Помилка")
 else:
     print(f'Периметр:{x+y+z}')
+    print(f'Площа:{s}')
+
+
     
 x=int(input("X = "))
 y=int(input("Y = "))
