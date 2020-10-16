548266760a18d8246caf7c9e6037cbb124882f50
Donskykh_Volodimir <71886261+VDblue@users.noreply.github.com>
2020-10-16 12:36:42 +0300

    Update sam-doskykh.py

--- Donskykh/sam-doskykh.py
+++ Donskykh/sam-doskykh.py
@@ -1,8 +1,10 @@
 x=float(input())
 y=float(input())
 z=float(input())
-
+from math import sqrt
 print(f'периметр такого трикутникак дорівнює:{x+y+z}')
+print(f'площа такого трикутникак дорівнює:{sqrt((x+y+z)/2*(x+y-z)/2*(x-y+z)/2*(-x+y+z)/2)}')
+
 
 x=float(input())
 y=float(input())
