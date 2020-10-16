6f2c7792e812a1285213c77e4fce2cf81c0f166a
korven4 <71665342+korven4@users.noreply.github.com>
2020-10-16 12:39:07 +0300

    Update sam_Vasilchenko.py

--- Vasiclhenko/sam_Vasilchenko.py
+++ Vasiclhenko/sam_Vasilchenko.py
@@ -1,3 +1,4 @@
+import math
 #3
 
 a = int(input("A"))
@@ -5,11 +6,16 @@ b = int(input("B"))
 c = int(input("C"))
 
 P=a+b+c
+l=P/2
+q=l*(l-a)*(l-b)*(l-c)
+S=math.sqrt(q)
 print(P)
+print(S)
 
 #4
 
 x=int(input("X"))
 y=int(input("Y"))
+c=int(input("C"))
 
-print(f'A[{x},{y}]')
+print(f'A[{x},{y},{c}]')
