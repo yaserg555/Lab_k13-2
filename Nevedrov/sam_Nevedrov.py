b80700d5a60b605eef1547f1405817bbaf2a2f87
Nevedrov <71889912+Nevedrov@users.noreply.github.com>
2020-10-16 13:13:26 +0300

    Update sam_Nevedrov.py

--- Nevedrov/sam_Nevedrov.py
+++ Nevedrov/sam_Nevedrov.py
@@ -1,9 +1,19 @@
 print('Алексей')
+#3
 a = int(input('a:'))
 b = int(input('b:'))
 c = int(input('c:'))
 p = a+b+c
 print(f'{p}')
+#4
 x = int(input('x:'))
 y = int(input('y:'))
 print(f'{x,y}')
+#6
+a = int(input('A:'))
+b = int(input('B:'))
+c = int(input('C:'))
+if a + b == c or b + c == a or a + c == b and a >= 0 and b >= 0 and c >= 0:
+    print('yes')
+else:
+    print('no')
