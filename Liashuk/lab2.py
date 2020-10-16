0a5cfb63308cf6850cc81ae8df535b96258e8b46
AndreyProgres <71766137+AndreyProgres@users.noreply.github.com>
2020-10-16 03:23:40 +0300

    Create lab2.py

---
+++ Liashuk/lab2.py
@@ -0,0 +1,16 @@
+print(2*4%7, type(2*4%7))
+print(2*(4%7), type(2*(4%7)))
+print(51//6%7, type(51//6%7))
+print(51//(6%7), type(51//(6%7)))
+print((-3+5)*(2%7//3+4*2), type((-3+5)*(2%7//3+4*2)))
+print((3//5.0)*20+32.0, type((3//5.0)*20+32.0))
+print(float(int(1.56)), type(float(int(1.56))))
+print(bool(-0.0), type(bool(-0.0)))
+print(float('inf'), type(float('inf')))
+print(int(bool(13.56)), type(int(bool(13.56))))
+print("""
+Виконавець: Ляшук
+            Андрій
+            Анатолійович
+     Група: К-13(2)
+""")
