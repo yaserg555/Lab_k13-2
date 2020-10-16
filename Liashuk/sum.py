1aa82f5d6e5f53154715bf258d50cd03f53939c2
AndreyProgres <71766137+AndreyProgres@users.noreply.github.com>
2020-10-16 13:36:54 +0300

    Update sum.py

--- Liashuk/sum.py
+++ Liashuk/sum.py
@@ -1,4 +1,4 @@
-try:
+try:                                   #3 AND 4          
     a = float(input("Уведіть довжини стороти 1: "))
     b = float(input("Уведіть довжини сторони 2: "))
     c = float(input("Уведіть довжини сторони 3: "))
@@ -8,3 +8,22 @@ try:
     print(f"[{x},{y}]")
 except:
     print("Введи число!!!")
+try:                                   #6
+    a=int(input("Число а:  "))
+    b=int(input("Число b:  "))
+    c=int(input("Число c:  "))
+    if a+b==c or a+c==b or c+b==a and  a >= 0 and b >= 0 and c >= 0:
+        print("Yes")
+    else:
+        print("No")
+except:
+    print("Введи число!!!")
+try:                                    #7
+    a1=int(input("Число a1:  "))
+    b1=int(input("Число b1:  "))
+    c1=int(input("Число c1:  "))
+    d1=int(input("Число d1:  "))
+    print(max({a1},{b1},{c1},{d1}))
+except:
+    print("not found")
+
