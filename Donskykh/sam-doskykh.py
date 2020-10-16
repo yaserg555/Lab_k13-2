ed65b9a46ddfb4287f86516b9e01fcd0bcd534c4
Donskykh_Volodimir <71886261+VDblue@users.noreply.github.com>
2020-10-16 13:17:46 +0300

    Update sam-doskykh.py

--- Donskykh/sam-doskykh.py
+++ Donskykh/sam-doskykh.py
@@ -18,3 +18,16 @@ z=float(input())
 if x+y==z or x+z==y or z+y==x:
     print('yes')
 
+    #7
+x=float(input())
+y=float(input())
+z=float(input())
+k=float(input())
+l=[x,y,z,k]
+l.sort()
+def y():
+    for i in[3,2,1,0]:
+        if l[i]%2 == 0:
+            return l[i]
+    return "not found"
+print(y())
