d77f3b94c110d505a43f4e3260dd2422efffb829
Donskykh_Volodimir <71886261+VDblue@users.noreply.github.com>
2020-10-22 15:39:29 +0300

    Update Dz_3.py

--- Donskykh/Dz_3.py
+++ Donskykh/Dz_3.py
@@ -11,3 +11,21 @@ x=float(input())
 y=float(input())
 z=float(input())
 print(f'{x!=y and y!=z and z!=x}')
+
+  #3
+def check(a,b,e:int =10):
+    return f"%.{e}f"%a ==f"%.{e}f"%b
+print(check(1.23,1.23,30))
+
+  #4
+def check(a:list,b=list):
+    return a[0]/b[0] == a[1]/b[1] ==a[2]/b[2]
+print('введи через запятую коефициэнты прямой 1')
+x=str(input())
+x=x.split(',')
+x=list(map(lambda x:float(x),x))
+print('введи через запятую коефициэнты прямой 2')
+y=str(input())
+y=y.split(',')
+y=list(map(lambda y:float(y),y))
+print(check(x,y))
