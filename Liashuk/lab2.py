9910f005c03f59a6f42947f9ca406b5e9657db7f
AndreyProgres <71766137+AndreyProgres@users.noreply.github.com>
2020-10-23 02:34:09 +0300

    Update lab2.py

--- Liashuk/lab2.py
+++ Liashuk/lab2.py
@@ -9,18 +9,22 @@ print(float(int(1.56)), type(float(int(1.56))))
 print(bool(-0.0), type(bool(-0.0)))
 print(float('inf'), type(float('inf')))
 print(int(bool(13.56)), type(int(bool(13.56))))
-print("""
 
 #№2
-def chislo(x):
+def chislo1(x):
     print(x%2)
-chislo(2132133)
+def chislo2(x):
+    print(hex(x%16))
+
+print("значення молодшої двійкової цифри числа:"),chislo1(2324544)
+print("значення молодшої шістнадцяткової цифри числа:"),chislo2(122312321)
 
 
 #№3
 print((10.001/9.001)**345*(13.001/11.001)**249/9.001**10/11.001**20)
 
 #№4
+print("""
 Виконавець: Ляшук
             Андрій
             Анатолійович
