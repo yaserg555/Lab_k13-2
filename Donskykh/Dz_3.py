73ffa01d91eb8bc96777556216216c80b61a0b4f
Donskykh_Volodimir <71886261+VDblue@users.noreply.github.com>
2020-10-22 14:01:33 +0300

    Update Dz_3.py

--- Donskykh/Dz_3.py
+++ Donskykh/Dz_3.py
@@ -1,2 +1,13 @@
 # https://www.codewars.com/users/VDblue
 # @Masinfuture002_bot
+
+  #1
+def privet(a,b):
+    return f'Добрий день,{a} {b}'
+print(privet('Володимир','Донських'))
+
+  #2
+x=float(input())
+y=float(input())
+z=float(input())
+print(f'{x!=y and y!=z and z!=x}')
