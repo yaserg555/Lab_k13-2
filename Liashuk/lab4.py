ea1a967b733443d51e608c956419f0e727e79d44
AndreyProgres <71766137+AndreyProgres@users.noreply.github.com>
2020-10-24 21:38:38 +0300

    Create lab4.py

---
+++ Liashuk/lab4.py
@@ -0,0 +1,34 @@
+# №1
+def hello(a,b):
+    return print(f"Привіт, {a} {b}")
+
+# №2
+def rizni():
+    x=input("x:  ")
+    y=input("y:  ")
+    z=input("z:  ")
+    return print(x!=y and x!=z and y!=z)
+
+# №4
+def kof1():
+    return (a1==a2 and b1==b2 and c1!=c2)
+def kof2():
+    return (a1==a2 and b1==b2 and c1==c2)
+def kof3():
+    return (a1!=a2 and b1!=b2)
+
+print("№1")
+hello('Ляшук','Андрій')
+
+print("№2")
+rizni()
+
+print("№4")
+a1, b1, c1 = int(input("a1: ")), int(input("b1: ")), int(input("c1: "))
+a2, b2, c2 = int(input("a2: ")), int(input("b2: ")), int(input("c2: "))
+kof1()
+kof2()
+kof3()
+print(f"Колінеарні:{kof1()}")
+print(f"Накладаються:{kof2()}")
+print(f"Перетинаються:{kof3()}")
