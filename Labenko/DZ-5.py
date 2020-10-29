c507867f2aa5ec8550a1dd006fe75f297415fed4
Annalab-sys <72043120+Annalab-sys@users.noreply.github.com>
2020-10-29 20:50:29 +0200

    Update DZ-5.py

--- Labenko/DZ-5.py
+++ Labenko/DZ-5.py
@@ -1,6 +1,6 @@
 # 1
 
-x = input('Впишіть літеру')
+x = input(f'Впишіть літеру')
 def vic(x):
     d = ['A', 'B', 'C', 'D', 'E', 'F']
     return x in d
@@ -30,11 +30,26 @@ def maths(a,b,c):
 print(f'Результат = {maths(a,b,c)}')
       
 # 4
-a = float(input("Довжина першого ребра цегли = "))
-b = float(input("Довжина другого ребра цегли = "))
-c = float(input("Довжина третього ребра цегли = "))
-x = float(input("Довжина вікна = "))
-y = float(input("Ширина вікна = "))
+a = float(input(f"Довжина першого ребра цегли = "))
+b = float(input(f"Довжина другого ребра цегли = "))
+c = float(input(f"Довжина третього ребра цегли = "))
+x = float(input(f"Довжина вікна = "))
+y = float(input(f"Ширина вікна = "))
 def vic(a, b, c, x, y):
     return (x > b and x > a and y > b and y > a) or (x > b and x > c and y > b and y > b) or (x > a and x > c and y > a and y > c)
 print(f'Чи влазить цегла у вікно? {vic(a, b, c, x, y)}')
+     
+# 5
+x = int(input(f'Скільки років?'))
+def years(x):
+    d = (x % 10)
+    if d == 1 and x != 11:
+        return 'рік'
+    elif (d == 2 or d == 4 or d == 3) and x not in range(11,15) :
+        return 'рoки'
+    elif d in range(5,10) or d == 0 or x in range(11,20):
+        return 'рoків'
+    else:
+        return 'error'
+print(f' {x} {years(x)}')
+    
