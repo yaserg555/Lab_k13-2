ad73d423cbdca090faf56812c23fad9f793c72b0
Nikita <45831329+gaspar62@users.noreply.github.com>
2020-10-29 18:29:57 +0200

    Update dz4.py

--- Gasparian/dz4.py
+++ Gasparian/dz4.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# https://www.codewars.com/users/lepsik/stats
 import math
 
 
