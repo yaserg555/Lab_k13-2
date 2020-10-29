8c237bfee22cc402ce4a6f22104238b0bbb08189
Annalab-sys <72043120+Annalab-sys@users.noreply.github.com>
2020-10-29 19:03:54 +0200

    Create DZ-5.py

---
+++ Labenko/DZ-5.py
@@ -0,0 +1,21 @@
+# 2
+
+def number(x):
+    if x < 0:
+        return -1
+    elif x == 0:
+        return 0
+    elif x > 0:
+        return 1
+print(f'Результат = {number(x)}')
+
+# 3
+def maths(a,b,c):
+    k = float((b**2)-4*a*c)
+    if k < 0:
+        return -1
+    elif k == 0:
+        return -b/2*a, b/2*a  
+    elif k > 0:
+        return float((-b+(sqrt(k))/(2*a))), float((-b-(sqrt(k))/(2*a)))
+print(f'Результат = {maths(a,b,c)}')
