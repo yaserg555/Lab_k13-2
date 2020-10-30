79d920150af488d974efcea1a701815563661e92
Nikita <45831329+gaspar62@users.noreply.github.com>
2020-10-30 10:26:10 +0200

    Update lab3.py

--- Gasparian/lab3.py
+++ Gasparian/lab3.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# https://www.codewars.com/users/lepsik/stats
 
 from math import tan, pi, atan, log, exp, cos, sin
 from decimal import Decimal
