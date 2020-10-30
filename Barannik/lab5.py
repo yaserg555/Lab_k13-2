687ed05788855c7f666c01aba70bfe41aa5d6795
Olga-Barannik <71884982+Olga-Barannik@users.noreply.github.com>
2020-10-30 12:31:55 +0200

    Update lab5.py

--- Barannik/lab5.py
+++ Barannik/lab5.py
@@ -1,6 +1,6 @@
 #1
 def cufra(x) :
-    return x=='0' or x=='1' or x=='2' or x=='3' or x=='4' or x=='5' or x=='6' or x=='7' or x=='8' or x=='9' or x=='A' or x=='B' or x=='C' or x=='D' or x=='E' or x=='F'
+    return x=='A' or x=='B' or x=='C' or x=='D' or x=='E' or x=='F'
 
 #2
 def znak(x):
