57246e4a387bdfe13157878b7068015a2afaef78
Annalab-sys <72043120+Annalab-sys@users.noreply.github.com>
2020-11-12 19:22:40 +0200

    Update Dz-7.py

--- Labenko/Dz-7.py
+++ Labenko/Dz-7.py
@@ -1,1 +1,15 @@
-
+#1 
+def odd():
+    a = int(input(f"\nа = "))
+    print(f"\nЧи містить число {a} непарні числа?")
+    c = abs(a)
+    d = 0
+    while c > 0:
+        if c % 2 != 0:
+            d += 1
+        c = c // 10
+    if d >= 1:
+        print(f"Так")
+    else:
+        print(f"Ні")
+odd()
