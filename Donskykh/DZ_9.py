e71433c8cc9d5cbf626e0d4da4269c582c68bef6
Donskykh_Volodimir <71886261+VDblue@users.noreply.github.com>
2020-11-24 15:59:49 +0200

    Create DZ_9.py

---
+++ Donskykh/DZ_9.py
@@ -0,0 +1,16 @@
+def f2():
+    s=list(range(2,20))
+    print(s[3:9])
+
+
+def f6(s):
+    print(f'1) {s[:len(s) // 2 + 1 * len(s) % 2]}')
+    print(f'2) {s[:len(s) // 2]}')
+    print(f'3) {s[:len(s) // 3]}')
+    print(f'4) {s[len(s) // 2:]}')
+    print(f'5) {s[len(s) // 2 + 1 * len(s) % 2:]}')
+    print(f'6) {s[len(s) // 3:2 * len(s) // 3]}')
+    print(f'7) {s[:10]}')
+    print(f'8) {list(reversed(s[:10]))}')
+    print(f'9) {s[2::3]}')
+    print(f'10) {list(reversed(s[2::3]))}')
