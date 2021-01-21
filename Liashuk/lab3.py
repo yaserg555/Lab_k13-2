f5898e7877772e7b5a6f147f91fc84e3b12358e3
AndreyProgres <71766137+AndreyProgres@users.noreply.github.com>
2021-01-21 18:32:35 +0200

    Update lab3.py

--- Liashuk/lab3.py
+++ Liashuk/lab3.py
@@ -1,3 +1,5 @@
+import datetime
+from math import cos,sin,e,tan,exp,log
 #№1
 x=1
 y=2.5
@@ -5,75 +7,54 @@ y=x
 x=3
 x+=x
 print(f"""x={x}; y={y}""")
-print("///////////////////////////////////////////\n")
+
 
 #№2
 x,y=[1,2]
 x,y,y,x=[y,x,3,y]
 print(f"""x={x}; y={y}""")
-print("///////////////////////////////////////////\n")
+
 
 #№3 and 4
 print(f"""x^2+y^2 ={(x+y)/(x**2+y**2)}
 x^2+y^2 ={round((x+y)/(x**2+y**2),3)}""")
-print("///////////////////////////////////////////\n")
 
-#№5
+
+# №5
 def Time():
     try:
-        x=int(input("Скільки годин:  "))
-        y=int(input("Скільки хвилин: "))
-        if 0<=x<10 and 0<=y<10:
-            print(f"""Time:  0{x}:0{y} """)
-        else:
-            if  0<=x<10 and 10<=y<60:
-                print(f"""Time:  0{x}:{y} """)
-            else:
-                if  10<=x<24 and  0<=y<10:
-                    print(f"""Time:  {x}:0{y} """)
-                else:
-                    if 10<=x<24 and 10<=y<60:
-                        print(f"""Time:  {x}:{y} """)
-                    else:
-                        if x<0 or x>23:
-                            print('Введи правильні дані в години!')
-                        else:
-                            if y<0 or y>59:
-                                print('Введи правильні дані у хвилини!')
-    except ValueError:
-        print("Введи число!!!")
-
+        hour=int(input("Скільки годин? "))
+        minute=int(input("Скільки хвилин? "))
+        a=datetime.time(hour,minute)
+        print(f'{a.hour}:{a.minute}')
+    except ValueError as e:
+        print(e)
 Time()
-print("///////////////////////////////////////////\n")
-from math import e,atan
-print(e)
-print("tg(34) =", atan(34))
-print("///////////////////////////////////////////\n")
-try:
-    dd=int(input("Day: "))
-    mm=int(input("Month: "))
-    yy=int(input("Year: "))
-    if (mm==1) and (dd>31) or (mm==2) and (dd>28) or (mm==3) and (dd> 31) or (mm==4) and (dd>30) or (mm==5) and (dd>31) or (mm==6) and (dd>30) or (mm==7) and (dd>31) or (mm==8) and (dd>31)  or (mm==9)  and (dd>30)  or (mm==10) and (dd>31) or (mm==11) and (dd>30 ) or (mm==12) and (dd>31 ) or (dd<1):
-        print("Ви ввели не правильні дані у \"Day\"")
-    else:
-        if (mm>12) or (mm<1):
-            print("Ви ввели не правильні дані у \"Month\"")
-        else:
-            if yy<2020:
-                print("Ви ввели не правильні дані у \"Year\"")
-            else:
-                print(f""" {dd}.{mm}.{yy} """)
-except ValueError:
-    print("Введіте число!!!")
-print("///////////////////////////////////////\n")
-try:
-    x=int(input("Введіте 'х' для обчислення ctg(x+y):  "))
-    y=int(input("Введіте 'y' для обчислення ctg(x+y):  "))
-    from math import cos, sin
-    print("ctg(x + y) =",'{:.2e}'.format(cos(x+y)/sin(x+y)))
-except ValueError:
-    print("ви ввели не правильні дані у 'x' or 'y'")
-except:
-    if x+y==0:
-        print("ctg 0, НЕ ІСНУЄ")
-print("////////////////////////////////////////\n")
+
+# №6
+print("tg(34) =", tan(34))
+print("e =", e)
+
+# №7
+def Date():
+    try:
+        day=int(input("Який день? "))
+        month=int(input("Який місяць? "))
+        year=int(input("Який рік? "))
+        a=datetime.date(year,month,day)
+        print(f'{a.day}.{a.month}.{a.year}')
+    except ValueError as e:
+        print(e)
+Date()
+
+# №8
+def ctg():
+    try:
+        x=int(input("Введіте 'х' для обчислення ctg(x+y):  "))
+        y=int(input("Введіте 'y' для обчислення ctg(x+y):  "))
+        print("ctg(x + y) =",'{:.2e}'.format(cos(x+y)/sin(x+y)))
+    except ValueError as e:
+        print(e)
+    except ZeroDivisionError as e:
+        print(e)
+ctg()
