5179e3d962c2fd4a0fad44cb6b06b7a4a4049c40
AndreyProgres <71766137+AndreyProgres@users.noreply.github.com>
2021-02-17 11:06:26 +0200

    Update lab11(2).py

--- Liashuk/lab11(2).py
+++ Liashuk/lab11(2).py
@@ -1,8 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+    return (x**3 + 2*x**2 - 3*x - 2) / (x**2 + x - 2)
+
+x = np.linspace(-4, 4, 500)
+A = f(x)
+plt.figure(figsize = (17,12), dpi = 80)
+
+A[A > 100] = np.inf
+A[A < -100] = -np.inf
+
+sp = plt.subplot(224)
+sp.spines['left'].set_position('center')
+sp.spines['right'].set_position('center')
+sp.spines['bottom'].set_position('center')
+sp.spines['top'].set_position('center')
 
 
-minimum = np.min(array, axis = 1)
-maxymum = np.max(array, axis = 0)
-for i in range(n):
-    for j in range(m):
-        if (minimum[i] == maxymum[j]):
-            print(f'{minimum[i]} ---> {(i,j)}')
+plt.plot(x,A, label = r'$f_1=\ (x^3 + 2x^2 - 3x - 2)/(x^2 + x - 2)$', color = 'red')
+plt.plot([-2,-2],[-10,10],[1,1],[-10,10], color = 'blue', linestyle = '--')
+
+
+plt.ylim((-10,10))
+plt.show()
+
