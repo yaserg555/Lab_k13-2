2c03ea9d682e7c021e775dcd66774a3c8f2f2bfc
OleksandraRyzhova0502 <71883743+OleksandraRyzhova0502@users.noreply.github.com>
2021-02-23 21:52:57 +0200

    Add files via upload

---
+++ Ryzhova/glasshouse.py
@@ -0,0 +1,18 @@
+from glasshouse import *
+
+glasshouse = Glasshouse()
+glasshouse.show_status()
+
+while glasshouse.state:
+    command = input('What do you want to change?\nWindows, heaters, t, humidity: ')
+    if command == 't':
+        glasshouse.change_temperature()
+    elif command == 'windows':
+        glasshouse.control_windows()
+    elif command == 'heaters':
+        glasshouse.control_heaters()
+    elif command == 'humidity':
+        glasshouse.change_relative_humidity()
+    else:
+        continue
+    glasshouse.show_status()
