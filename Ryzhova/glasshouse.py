af257a030314b1d56cad2497fe89f9b52a2d8512
OleksandraRyzhova0502 <71883743+OleksandraRyzhova0502@users.noreply.github.com>
2021-02-24 12:04:58 +0200

    Update glasshouse.py

--- Ryzhova/glasshouse.py
+++ Ryzhova/glasshouse.py
@@ -7,12 +7,14 @@ while glasshouse.state:
     command = input('What do you want to change?\nWindows, heaters, t, humidity: ')
     if command == 't':
         glasshouse.change_temperature()
+        glasshouse.protector()
     elif command == 'windows':
         glasshouse.control_windows()
     elif command == 'heaters':
         glasshouse.control_heaters()
     elif command == 'humidity':
         glasshouse.change_relative_humidity()
+        glasshouse.protector()
     else:
         continue
     glasshouse.show_status()
